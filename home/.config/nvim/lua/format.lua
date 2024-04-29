b240a8acd43ab4c9f9fab9f11ec97a19370a9c9d
Shardul Baral <shardul@baral.ca>
2024-04-28 21:19:09 -0400

    .

--- home/.config/nvim/lua/format.lua
+++ home/.config/nvim/lua/format.lua
@@ -1,6 +1,5 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("gitsigns").setup()
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.ocamlformat,
