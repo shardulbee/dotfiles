a2a50d12a84d094b233741a967745f0137ebd657
Shardul Baral <shardul@distributional.com>
2025-02-18 20:56:14 -0500

    init good

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -13,10 +13,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	"direnv/direnv.vim", -- Integration with direnv for environment management
-	"tpope/vim-eunuch", -- Unix shell commands integration
-	"tpope/vim-surround", -- Surroundings manipulation (parentheses, brackets, etc)
-	"tpope/vim-sleuth", -- Automatic indentation detection
+	"tpope/vim-eunuch",
+	"tpope/vim-surround",
+	"tpope/vim-sleuth",
 	"tpope/vim-unimpaired",
 	"tpope/vim-repeat",
 	{
@@ -31,6 +30,7 @@ require("lazy").setup({
 			vim.opt.statusline = "%f:%l:%c %m%=%{FugitiveStatusline()} %y"
 		end,
 	},
+	"direnv/direnv.vim",
 	{
 		"tpope/vim-rhubarb",
 		event = "VeryLazy",
@@ -38,7 +38,6 @@ require("lazy").setup({
 			{ "<leader>gb", "<cmd>GBrowse<cr>" },
 		},
 	},
-
 	{ "echasnovski/mini.surround", version = false, config = true },
 	{ "echasnovski/mini.pairs", version = false, config = true },
 	{ "echasnovski/mini.splitjoin", version = false, config = true },
@@ -62,9 +61,6 @@ require("lazy").setup({
 						hidden = true,
 					},
 				},
-				oldfiles = {
-					cwd_only = true,
-				},
 				git = {
 					commits = {
 						winopts = { preview = { vertical = "down:60%" } },
@@ -83,15 +79,13 @@ require("lazy").setup({
 
 			local opts = { noremap = true, silent = true }
 			vim.keymap.set("n", "<C-t>", fzfLua.files, opts)
-			vim.keymap.set("n", "<A-t>", fzfLua.oldfiles, opts)
-			vim.keymap.set("n", "<leader>hh", fzfLua.help_tags, opts)
+			vim.keymap.set("n", "<leader>h", fzfLua.help_tags, opts)
 			vim.keymap.set("n", "<leader>b", fzfLua.buffers, opts)
 			vim.keymap.set("n", "<leader>f", fzfLua.blines, opts)
 			vim.keymap.set("n", "<leader>F", fzfLua.live_grep_native, opts)
 			vim.keymap.set("n", "<leader>r", fzfLua.command_history, opts)
 			vim.keymap.set("n", "<leader>p", fzfLua.commands, opts)
 
-			vim.keymap.set("n", "<leader>gl", fzfLua.git_commits, opts)
 			vim.keymap.set("n", "<leader>co", fzfLua.git_branches, opts)
 		end,
 	},
@@ -109,27 +103,14 @@ require("lazy").setup({
 			vim.cmd([[colorscheme gruvbox]])
 		end,
 	},
-
 	{
 		"saghen/blink.cmp",
-		-- use a release tag to download pre-built binaries
 		version = "v0.9.3",
-
 		opts = {
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
 			keymap = { preset = "enter" },
-
 			appearance = {
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
 			},
-
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
 				default = { "lsp", "path", "buffer" },
 				cmdline = {},
@@ -286,20 +267,22 @@ require("lazy").setup({
 							buf_set_keymap("n", "gd", vim.lsp.buf.definition, opts)
 							buf_set_keymap("n", "gr", vim.lsp.buf.references, opts)
 							buf_set_keymap("n", "cd", vim.lsp.buf.rename, opts)
+
 							buf_set_keymap("n", "]d", function()
 								vim.diagnostic.goto_next({ float = true, severity = vim.diagnostic.severity.ERROR })
 							end, opts)
 							buf_set_keymap("n", "[d", function()
 								vim.diagnostic.goto_prev({ float = true, severity = vim.diagnostic.severity.ERROR })
 							end, opts)
+							buf_set_keymap("n", "]D", function()
+								vim.diagnostic.goto_next({ float = true })
+							end, opts)
+							buf_set_keymap("n", "[D", function()
+								vim.diagnostic.goto_prev({ float = true })
+							end, opts)
 						end,
 					})
 				end,
-				-- Next, you can provide a dedicated handler for specific servers.
-				-- For example, a handler override for the `rust_analyzer`:
-				-- ["rust_analyzer"] = function ()
-				--     require("rust-tools").setup {}
-				-- end
 			})
 			vim.diagnostic.config({ virtual_text = false })
 		end,
@@ -369,7 +352,6 @@ require("lazy").setup({
 	},
 }, {})
 
--- Settings section {{{
 vim.cmd([[filetype plugin indent on]])
 vim.cmd([[syntax on]])
 vim.opt.wrap = false -- soft wrap off
@@ -408,9 +390,6 @@ vim.opt.termguicolors = true
 vim.opt.title = true
 vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
 
--- }}}
-
--- Create autocommand for trimming whitespace on save
 vim.api.nvim_create_augroup("TRIM_WHITESPACE", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = "TRIM_WHITESPACE",
@@ -418,7 +397,6 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	command = [[%s/\s\+$//e]],
 })
 
--- Create autocommand to equalize window sizes when terminal is resized
 vim.api.nvim_create_augroup("RESIZE_NVIM", { clear = true })
 vim.api.nvim_create_autocmd({ "VimResized" }, {
 	group = "RESIZE_NVIM",
@@ -428,111 +406,35 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 	end,
 })
 
--- Define keymap to open current file in Zed
-vim.keymap.set("n", "<leader><space>", function()
-	-- Helper function to find the git root directory
-	local function find_git_root()
-		local current = vim.fn.expand("%:p:h")
-		while current ~= "/" do
-			if vim.fn.isdirectory(current .. "/.git") == 1 then
-				return current
-			end
-			current = vim.fn.fnamemodify(current, ":h")
-		end
-		return nil
-	end
-
-	-- Get the git root directory
-	local git_root = find_git_root()
-	if not git_root then
-		print("No git root found")
-		return
-	end
-
-	-- Get current file path and cursor position
-	local file = vim.fn.expand("%:p")
-	local line = vim.fn.line(".")
-	local col = vim.fn.col(".")
-	-- Open file in Zed at current cursor position
-	vim.fn.system({ "zed", git_root, file .. ":" .. line .. ":" .. col })
-end, { noremap = true, silent = true })
-
--- Toggle quickfix window
-local function toggle_quickfix()
-	local qf_exists = false
-	for _, win in pairs(vim.fn.getwininfo()) do
-		if win.quickfix == 1 and win.loclist == 0 then
-			qf_exists = true
-		end
-	end
-	if qf_exists then
-		vim.cmd("cclose")
-	else
-		vim.cmd("copen")
-	end
-end
-
--- Toggle location list window
-local function toggle_loclist()
-	local loc_exists = false
-	for _, win in pairs(vim.fn.getwininfo()) do
-		if win.quickfix == 1 and win.loclist == 1 then
-			loc_exists = true
-		end
-	end
-	if loc_exists then
-		vim.cmd("lclose")
-	else
-		vim.cmd("lopen")
-	end
-end
-
--- Set the keymaps
-vim.keymap.set("n", "<leader>q", toggle_quickfix, { silent = true, desc = "Toggle quickfix" })
-vim.keymap.set("n", "<leader>l", toggle_loclist, { silent = true, desc = "Toggle loclist" })
-
 vim.keymap.set("n", "<leader>d", function()
 	vim.api.nvim_buf_delete(0, {})
 end)
-
-vim.api.nvim_create_user_command("Rebase", function()
-	vim.cmd("Git fetch origin")
-	vim.cmd("Git rebase origin/main")
-	local current_branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
-	vim.cmd(string.format("Git push origin %s --force-with-lease", current_branch))
-end, {})
-
--- Function to add Python debugger breakpoint with proper indentation
-local function add_python_breakpoint()
-	local pos = vim.api.nvim_win_get_cursor(0)
-	local line = pos[1] - 1
-	-- Get the indentation of the current line
-	local current_line = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
-	local indent = current_line:match("^%s*")
-
-	vim.api.nvim_buf_set_lines(0, line, line, false, {
-		indent .. "import ipdb",
-		indent .. "ipdb.set_trace()",
-	})
-end
-
--- Function to remove all Python debugger breakpoints
-local function remove_python_breakpoints()
-	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-	local new_lines = {}
-	for _, line in ipairs(lines) do
-		if not line:match("ipdb") then
-			table.insert(new_lines, line)
-		end
-	end
-	vim.api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
-end
 
 -- Set up autocommand to create mappings only for Python files
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "python",
 	callback = function()
-		vim.keymap.set("n", "<leader>9", add_python_breakpoint, { buffer = true })
-		vim.keymap.set("n", "<leader>0", remove_python_breakpoints, { buffer = true })
+		vim.keymap.set("n", "<leader>9", function()
+			local pos = vim.api.nvim_win_get_cursor(0)
+			local line = pos[1] - 1
+			-- Get the indentation of the current line
+			local current_line = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
+			local indent = current_line:match("^%s*")
+
+			vim.api.nvim_buf_set_lines(0, line, line, false, {
+				indent .. "import ipdb",
+				indent .. "ipdb.set_trace()",
+			})
+		end, { buffer = true })
+		vim.keymap.set("n", "<leader>0", function()
+			local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+			local new_lines = {}
+			for _, line in ipairs(lines) do
+				if not line:match("ipdb") then
+					table.insert(new_lines, line)
+				end
+			end
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
+		end, { buffer = true })
 	end,
 })
