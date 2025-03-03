0bf59cd9ce7040bc1253b772d19cff5cf030c2b3
Shardul Baral <shardul@distributional.com>
2025-03-03 08:49:25 -0500

    add codecompanion

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -61,6 +61,61 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"olimorris/codecompanion.nvim",
+		config = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								order = 1,
+								mapping = "parameters",
+								type = "enum",
+								desc = "ID of the model to use. See the model endpoint compatibility table for details on which models work with the Chat API.",
+								---@type string|fun(): string
+								default = "claude-3.5-sonnet",
+								choices = {
+									["o3-mini-2025-01-31"] = { opts = { can_reason = true } },
+									["o1-2024-12-17"] = { opts = { can_reason = true } },
+									["o1-mini-2024-09-12"] = { opts = { can_reason = true } },
+									"claude-3.5-sonnet",
+									"claude-3.7-sonnet",
+									"claude-3.7-sonnet-thought",
+									"gpt-4o-2024-08-06",
+									"gemini-2.0-flash-001",
+								},
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					slash_commands = {
+						["file"] = {
+							opts = { provider = "fzf_lua" },
+						},
+						["symbols"] = {
+							opts = { provider = "fzf_lua" },
+						},
+						["buffer"] = {
+							opts = { provider = "fzf_lua" },
+						},
+					},
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				},
+			},
+		},
+	},
 	"direnv/direnv.vim",
 	"tpope/vim-eunuch",
 	"tpope/vim-surround",
@@ -157,6 +212,9 @@ require("lazy").setup({
 				nerd_font_variant = "mono",
 			},
 			sources = {
+				per_filetype = {
+					codecompanion = { "codecompanion" },
+				},
 				default = { "lsp", "path", "buffer" },
 				cmdline = {},
 			},
