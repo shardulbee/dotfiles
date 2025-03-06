4c080a491ea501238255964a58b1a79860519d30
Shardul Baral <shardul@distributional.com>
2025-03-06 13:45:52 -0500

    Tmux?

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -14,6 +14,25 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	"preservim/vimux",
+	{
 		"mfussenegger/nvim-lint",
 		config = function()
 			require("lint").linters_by_ft = {
