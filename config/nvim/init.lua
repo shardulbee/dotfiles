f5d5e035a71322685b318bbd78838241f702830a
Shardul Baral <shardul@baral.ca>
2026-04-23 15:35:07 -0400

    leader space

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -63,4 +63,86 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+map("n", "<leader><space>", function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.bo[buf].bufhidden = "wipe"
+
+  local width = 60
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = 1,
+    col = math.floor((vim.o.columns - width) / 2),
+    row = math.floor((vim.o.lines - 1) / 2) - 1,
+    style = "minimal",
+    border = "rounded",
+    title = " pi ",
+    title_pos = "center",
+  })
+
+  vim.cmd("startinsert!")
+
+  local function close()
+    if vim.api.nvim_win_is_valid(win) then
+      vim.api.nvim_win_close(win, true)
+    end
+  end
+
+  local function submit()
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+    local prompt = table.concat(lines, "\n"):gsub("^%s+", ""):gsub("%s+$", "")
+    close()
+    if prompt == "" then return end
+
+    local file = vim.fn.expand("%:p")
+    local args = file ~= "" and { "pi", "-p", "@" .. file .. " " .. prompt } or { "pi", "-p", prompt }
+
+    vim.notify("pi: " .. prompt, vim.log.levels.INFO)
+
+    local stdout = {}
+    local stderr = {}
+
+    vim.fn.jobstart(args, {
+      stdout_buffered = true,
+      stderr_buffered = true,
+      on_stdout = function(_, data)
+        for _, line in ipairs(data) do
+          table.insert(stdout, line)
+        end
+      end,
+      on_stderr = function(_, data)
+        for _, line in ipairs(data) do
+          table.insert(stderr, line)
+        end
+      end,
+      on_exit = function(_, code)
+        vim.schedule(function()
+          if code ~= 0 then
+            local err = table.concat(vim.tbl_filter(function(l) return l ~= "" end, stderr), "\n")
+            vim.notify("pi failed: " .. (err ~= "" and err or "unknown error"), vim.log.levels.ERROR)
+            return
+          end
+          local out = table.concat(vim.tbl_filter(function(l) return l ~= "" end, stdout), "\n")
+          if out ~= "" then
+            local out_buf = vim.api.nvim_create_buf(false, true)
+            vim.bo[out_buf].buftype = "nofile"
+            vim.bo[out_buf].modifiable = true
+            vim.api.nvim_buf_set_lines(out_buf, 0, -1, false, vim.split(out, "\n"))
+            vim.bo[out_buf].modifiable = false
+            vim.cmd("botright 10new")
+            vim.api.nvim_win_set_buf(0, out_buf)
+            vim.notify("pi done", vim.log.levels.INFO)
+          else
+            vim.notify("pi done", vim.log.levels.INFO)
+            vim.cmd("checktime")
+          end
+        end)
+      end,
+    })
+  end
+
+  vim.keymap.set("i", "<CR>", submit, { buffer = buf, nowait = true })
+  vim.keymap.set("n", "<CR>", submit, { buffer = buf, nowait = true })
+  vim.keymap.set({ "n", "i" }, "<Esc>", close, { buffer = buf })
+end)
 map("n", "<leader>y", function() vim.fn.setreg("+", vim.fn.expand("%")) end)
