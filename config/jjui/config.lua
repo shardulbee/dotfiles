e2e6274a0ec9c32557c553038d6ccbc76ce5bb7e
Shardul Baral <shardul@baral.ca>
2026-04-30 21:58:20 -0400

    jjui: force light theme, bypass terminal detection

--- config/jjui/config.lua
+++ config/jjui/config.lua
@@ -84,8 +84,5 @@ function setup(config)
 
     config.ui = config.ui or {}
     config.ui.set_window_title = false
-    config.ui.theme = {
-        dark = "dark",
-        light = "light",
-    }
+    config.ui.theme = "light"
 end
