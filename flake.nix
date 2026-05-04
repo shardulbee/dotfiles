45dd27bb73692b99883a3027fe669c94f312ea4f
Shardul Baral <shardul@baral.ca>
2026-05-04 14:27:53 +0000

    Add nix flake for cross-platform dev env; fix setup on Linux; make gh credential helper portable

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+      forAll = f: nixpkgs.lib.genAttrs systems (sys: f nixpkgs.legacyPackages.${sys});
+    in {
+      packages = forAll (pkgs: {
+        dev = pkgs.buildEnv {
+          name = "dev";
+          paths = with pkgs; [
+            fish
+            git
+            neovim
+            tmux
+            htop
+            fd
+            ripgrep
+            bat
+            fzf
+            jq
+            gh
+            yazi
+            zoxide
+            atuin
+            hyperfine
+            jujutsu
+            nodejs
+            bun
+            uv
+            lua-language-server
+            direnv
+          ];
+        };
+      });
+    };
+}
