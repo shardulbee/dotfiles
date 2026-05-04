d60ed16fe48046fd35f1e8013fc345b38019ee9a
Shardul Baral <shardul@baral.ca>
2026-05-04 12:16:49 -0400

    jjui dark/light

--- config/jjui/config.lua
+++ config/jjui/config.lua
@@ -84,5 +84,8 @@ function setup(config)
 
     config.ui = config.ui or {}
     config.ui.set_window_title = false
-    config.ui.theme = "light"
+    config.ui.theme = {
+        dark = "dark",
+        light = "light",
+    }
 end
