bf8405f0cefe08ccce16f26abac5aeee850dbb69
Shardul Baral <shardul@baral.ca>
2026-07-31 11:25:36 -0400

    Improve markdown wrapping and navigation

--- nvim-init.lua
+++ nvim-init.lua
@@ -37,6 +37,17 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(args)
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+    vim.keymap.set("n", "j", "gj", { buffer = args.buf })
+    vim.keymap.set("n", "k", "gk", { buffer = args.buf })
+  end,
+})
+
 local fzflua = require("fzf-lua")
 local map = vim.keymap.set
 map("n", "<leader>g", "<cmd>Goyo 80<cr>", { desc = "toggle Goyo" })
